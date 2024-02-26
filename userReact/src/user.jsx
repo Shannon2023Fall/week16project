d73065d43c22d75452a10f0f1ccf606b0f4a5fb8
Shannon2023Fall <148981251+Shannon2023Fall@users.noreply.github.com>
2024-02-26 15:51:13 +0100

    Update user.jsx

--- userReact/src/user.jsx
+++ userReact/src/user.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation.jsx';
-import Home from './Home.jsx';
-import Blog from './Blog.jsx';
-import News from './News.jsx';
+import Home from './pages/Home.jsx';
+import Blog from './pages/Blog.jsx';
+import News from './pages/News.jsx';
+import Footer from './components/Footer.jsx';
+
 /* import './user.css' */
 export default function App() {
 return (
@@ -17,6 +19,7 @@ return (
     </Route>
   </Routes>
   </BrowserRouter>
+  <Footer />
 )
 };
 
